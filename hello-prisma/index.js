21527dacf1a949b4a6b06a8247ede860a8faab83
Monika ghimire <monika.ghimirey@boosoft.com.au>
2025-04-26 11:18:19 +0545

    curd flow done

--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -3,31 +3,71 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const main = async () => {
+    // single user add------------
     // const user = await prisma.user.create({
     //     data: {
     //         name: 'mlice',
     //         email: 'mhimirey@gmail.com'
     //     }
     // });
+    // console.log(user);
 
 
 
-    const Newuser = await prisma.user.createMany({
-        data: [
+    // multiple user add------------
+    // const Newuser = await prisma.user.createMany({
+    //     data: [
+    //         {
+    //             name: 'mlice',
+    //             email: 'salina.ghimirey@gmail.com'
+    //         },
+    //         {
+    //             name: 'monika',
+    //             email: 'monika.ghimirey@gmail.com'
+    //         },
+    //     ]
+    // });
+    // console.log(Newuser);
 
-            {
-                name: 'mlice',
-                email: 'salina.ghimirey@gmail.com'
-            },
 
-            
-            {
-                name: 'monika',
-                email: 'monika.ghimirey@gmail.com'
-            },
-        ]
+
+    // read fetch data -- get all users----------
+     const users = await prisma.user.findMany();
+    // console.log(users);
+
+    // read fetch data -- get by users id
+    //  const users = await prisma.user.findUnique({
+    //     where: {id:6}
+    //  });
+    // console.log(users);
+
+
+
+    // read fetch data -- get by users name-----------
+    //  const users = await prisma.user.findMany({
+    //     where: {name:'monika'}
+    //  });
+    // console.log(users);
+
+
+    //for update user----------------
+    // const updateUser = await prisma.user.update({
+    //     where: { id: 6 },
+    //     data: {
+    //         name: 'monika',
+    //         email: 'monika22@gmail.com'
+    //     }
+    // });
+    // console.log(updateUser)
+
+
+     //for delte user single----------------
+     const updateUser = await prisma.user.deleteMany({
+        where: { id: 3 },
+        
     });
-    console.log(Newuser);
+    console.log(users)
+
 };
 
 main()
